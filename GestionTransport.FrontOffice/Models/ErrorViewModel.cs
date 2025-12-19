6edc588401a8bbd21720b47ebd977d5b3384f5d7
Rota <rota.dev@et.in>
2025-12-19 15:57:46 +0300

    Initialisation du projet MVC .Net

---
+++ GestionTransport.FrontOffice/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GestionTransport.FrontOffice.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
